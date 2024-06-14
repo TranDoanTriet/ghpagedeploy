9306aa12655dd4678800026e9ebb5636b206c2c2
TranDoanTriet <trandoantriet@gmail.com>
2024-06-14 13:20:25 +0700

    update test case

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,22 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders edit src/App.js text', () => {
+  render(<App />);
+  const editText = screen.getByText(/edit src\/App.js/i);
+  expect(editText).toBeInTheDocument();
+});
+
+test('renders hello text', () => {
+  render(<App />);
+  const helloText = screen.getByText(/hello/i);
+  expect(helloText).toBeInTheDocument();
+});
+
+test('renders image with alt text', () => {
+  render(<App />);
+  const imgElement = screen.getByAltText(/logo/i);
+  expect(imgElement).toBeInTheDocument();
+});
+
